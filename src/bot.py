ae3e4e5c976fd816111f3aaeb03ca1cdd593cd47
“viviangit config --global user.name “Your_Name”git config --global user.name “vivian <vivian.yn.chang@gmail.com>
2024-05-13 17:46:37 -0500

    Finished bot.py Milestone 3 requirements

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,21 @@
 import sys,random
-from base import Cell, BlokusBase
-from fakes import BlokusFake
 from piece import Point, Piece, Shape, ShapeKind
 from blokus import Blokus
+import click
 
 # Classes that represent players with different strategies
 
 class Player:
 
-    def __init__(self, bot_game: BlokusFake, player: int):
+    """
+    A class that represents a Blokus player using either the N (needs improvement),
+    S (satisfactory), or U (unsatisfactory) strategy.
+    """
+
+    _bot_gme: Blokus
+    _player: int
+
+    def __init__(self, bot_game: Blokus, player: int):
         self._bot_game = bot_game
         self._player = player
 
@@ -32,7 +39,7 @@ class Player:
         Make a move using a piece that was determined by a specific strategy.
         """
         if not self.retired:
-            avail_moves: set[Piece] = self._bot_game.available_moves()
+            avail_moves: set[Piece] = self._bot_game.fake_available_moves()
             if avail_moves == set():
                 self._bot_game.retire()
             else:
@@ -41,7 +48,12 @@ class Player:
 
 class NBot(Player):
 
-    def __init__(self, bot_game: BlokusFake, player: int):
+    """
+    A Blokus player that uses a "needs improvement" strategy by randomly selecting
+    pieces to play.
+    """
+
+    def __init__(self, bot_game: Blokus, player: int):
         super().__init__(bot_game, player)
 
     def strategy(self, avail_moves: set[Piece]) -> Piece:
@@ -53,8 +65,12 @@ class NBot(Player):
     
 
 class SBot(Player):
+    """
+    A Blokus player that uses a "satisfactory" strategy by playing the largest
+    piece possible.
+    """
 
-    def __init__(self, bot_game: BlokusFake, player: int):
+    def __init__(self, bot_game: Blokus, player: int):
         super().__init__(bot_game, player)
     
     def strategy(self, avail_moves: set[Piece]) -> Piece:
@@ -74,35 +90,90 @@ class SBot(Player):
     def make_move(self) -> None:
         super().make_move()
 
-# Simulate games
+class UBot(Player):
 
-num_games: int = int(sys.argv[1])
+    """
+    A Blokus player that uses an "unsatisfactory" strategy by playing the smallest
+    piece possible.
+    """
 
-one_wins: int = 0
-two_wins: int = 0
-ties: int = 0
+    def __init__(self, bot_game: Blokus, player: int):
+        super().__init__(bot_game, player)
+    
+    def strategy(self, avail_moves: set[Piece]) -> Piece:
+        return self.smallest_piece(avail_moves)
 
-for i in range(num_games):
+    def smallest_piece(self, avail_moves: set[Piece]) -> Piece:
+        """
+        Find a piece with the smallest size out of the available pieces.
+        """
 
-    start_positions: set[Point] = set([(0,0),(10,10)])
+        shape_sizes: dict[int,Piece] = {}
+        for piece in avail_moves:
+            shape_sizes[len(piece.shape.squares)] = piece
+        min_size: int = min(shape_sizes)
+        return shape_sizes[min_size]
+    
+    def make_move(self) -> None:
+        super().make_move()
 
-    bot_game: BlokusFake = BlokusFake(2, 11, start_positions)
+# Set up command line interface
 
-    bot1: NBot = NBot(bot_game, 1)
-    bot2: SBot = SBot(bot_game, 2)
+@click.command()
+@click.option("-n","--num-games",
+              type=int,
+              default=20)
+@click.option("-1", "--player1",
+              type=click.Choice(["S", "N", "U"]),
+              default="N")
+@click.option("-2", "--player2",
+              type=click.Choice(["S", "N", "U"]),
+              required=True)
+def cmd(num_games: int, player1: str, player2: str) -> None:
 
-    while not bot_game.game_over:
-        bot1.make_move()
-        bot2.make_move()
-               
-    if bot_game.winners is not None:
-        if len(bot_game.winners) > 1:
-            ties += 1
-        elif bot_game.winners[0] == 1:
-            one_wins += 1
+    # Simulate games
+
+    one_wins: int = 0
+    two_wins: int = 0
+    ties: int = 0
+
+    for i in range(num_games):
+
+        start_positions: set[Point] = set([(0,0),(10,10)])
+
+        bot_game: Blokus = Blokus(2, 11, start_positions)
+
+        if player1 == "S":
+            bot1: SBot = SBot(bot_game, 1)
+        elif player1 == "N":
+            bot1: NBot = NBot(bot_game, 1)
         else:
-            two_wins += 1
+            bot1: UBot = UBot(bot_game, 1)
 
-print(f"Bot 1 Wins |  {one_wins/num_games*100:.2f} %")
-print(f"Bot 2 Wins |  {two_wins/num_games*100:.2f} %")
-print(f"Ties       |  {ties/num_games*100:.2f} %")
+        if player2 == "S":
+            bot2: SBot = SBot(bot_game, 2)
+        elif player2 == "N":
+            bot2: NBot = NBot(bot_game, 2)
+        else:
+            bot2: UBot = UBot(bot_game, 2) 
+
+        while not bot_game.game_over:
+            bot1.make_move()
+            bot2.make_move()
+                
+        if bot_game.winners is not None:
+            if len(bot_game.winners) > 1:
+                ties += 1
+            elif bot_game.winners[0] == 1:
+                one_wins += 1
+            else:
+                two_wins += 1
+
+    print(f"Bot 1 ({player1}) Wins |  {one_wins/num_games*100:.2f} %")
+    print(f"Bot 2 ({player2}) Wins |  {two_wins/num_games*100:.2f} %")
+    print(f"Ties       |  {ties/num_games*100:.2f} %")
+
+if __name__ == "__main__":
+    cmd()
+
+
